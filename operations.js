248e017b93e4e98f50aeafc0917bf1a74883be84
Akshit Aman <amanakshit1234@gmail.com>
2025-07-15 23:36:13 +0530

    Learned operations

---
+++ operations.js
@@ -0,0 +1,32 @@
+let score = 103
+let bonus = 105
+
+let TotalScore = score + bonus
+console.log(TotalScore);
+
+
+// other way
+
+let addition = 29 +23
+console.log(addition)
+
+let subtraction = 49 - 53
+console.log(subtraction)
+
+
+let MyScore = 117
+MyScore++
+console.log(MyScore)
+
+let credit = 235
+credit--
+console.log(credit)
+
+
+//                  comparison operations
+let num1 = 55
+let num2 = 23
+let num3 = 39
+console.log(num1 == num2)
+console.log(num3 != num2)
+console.log(num1 > num2)
