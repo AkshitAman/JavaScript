bed77fb85612c95fba412d24f56372128bef767f
Akshit Aman <amanakshit1234@gmail.com>
2025-07-15 23:42:14 +0530

    learned about logical operators

---
+++ logical.js
@@ -0,0 +1,14 @@
+// && -> and operatior
+// || -> or operator
+//  ! -> reverse operator
+
+let isLoggedin = true
+let paid = false
+
+console.log(isLoggedin && paid)
+
+
+let VerifyEmail = true
+let VerifyNumb = false
+
+console.log(VerifyEmail || VerifyNumb)
