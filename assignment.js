b45388411d446aa76d24b03fd53ed7e2f43022fc
Akshit Aman <amanakshit1234@gmail.com>
2025-07-19 00:07:03 +0530

    Learned the concept of operator Presidence

---
+++ assignment.js
@@ -0,0 +1,16 @@
+let num1 = 17
+num1 += 5
+console.log(num1)
+
+
+
+//                  Operators Presidence(OpePre)
+
+let calc = 3 * 3 + 5 - 9
+console.log(calc)
+
+
+//                  Other Way
+
+let Equa = ((3 * (3 + 5)) - 9)
+console.log(Equa)
