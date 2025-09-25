329efd3d1bde6b3a65a7e216ed5d0d4620c0b862
Akshit Aman <amanakshit1234@gmail.com>
2025-09-25 23:43:06 +0530

    Weather App

--- Weather_App/script.js
+++ Weather_App/script.js
@@ -1,13 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const cityInput = document.addEventListener("city-input");
-  const getWeatherButn = document.addEventListener("Get-weather-button");
-  const weatherInfo = document.addEventListener("weather-info");
-  const cityNameDisplay = document.addEventListener("city-name");
-  const temperature = document.addEventListener("temperature");
-  const description = document.addEventListener("description");
-  const errorDisplay = document.addEventListener("error-display");
+  const cityInput = document.getElementById("city-input");
+  const getWeatherButn = document.getElementById("get-weather-button");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityNameDisplay = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-message");
 
-  getWeatherButn.addEventListener("click", () => {
-    cityInput.value.trim();
+  const API_KEY = "3901c6440b82af8339249d40a72a6a95";
+
+  getWeatherButn.addEventListener("click", async () => {
+    const city = cityInput.value.trim();
+    if (!city) return;
+
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
   });
+
+  async function fetchWeatherData(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("City not found");
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+  function displayWeatherData(data) {
+    console.log(data);
+    const { name, main, weather } = data;
+    cityNameDisplay.textContent = name;
+    temperatureDisplay.textContent = `Temperature : ${main.temp}`;
+    descriptionDisplay.textContent = `Weather : ${weather[0].description}`;
+
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+  }
+
+  function showError() {
+    weatherInfo.classList.add("hidden");
+    errorMessage.classList.remove("hidden");
+  }
 });
